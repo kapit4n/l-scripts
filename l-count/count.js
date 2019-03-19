d1a76f635c38b03d819eadb39b3f1873c352fb79
Luis Arce <luis.arce22@gmail.com>
2019-03-19 13:21:11 -0400

    Update count.js

--- l-count/count.js
+++ l-count/count.js
@@ -2,14 +2,17 @@ const fs = require('fs');
 const srcFilejs = './src/js';
 const srcFileJava = './src/java';
 const srcFileScala = './src/scala';
+const srcFileDotnet = './src/dotnet';
+const srcFileWords = './src/words';
 
-const countLines = function(filePath, callback) {
+const countLines = function (filePath, callback) {
     let i;
     let count = 0;
     fs.createReadStream(filePath)
         .on('error', e => callback(e))
         .on('data', chunk => {
-            for (i=0; i < chunk.length; ++i) if (chunk[i] == 10) count++;
+            for (i = 0; i < chunk.length; ++i)
+                if (chunk[i] == 10) count++;
         })
         .on('end', () => callback(null, count));
 };
@@ -17,28 +20,38 @@ const countLines = function(filePath, callback) {
 var totalCountsJs = [];
 var totalCountsJava = [];
 var totalCountsScala = [];
+var totalCountsDotnet = [];
+var totalCountsWords = [];
 
 function countLinesFiles(srcFile, files, collection) {
     files.forEach(f => {
-        countLines(srcFile + "/" + f, function(err, data) {
+        countLines(srcFile + "/" + f, function (err, data) {
             collection.push(data + 1);
         });
     })
 }
 
-function countFileByLanguage(srcFile, collection) {
+function countFileByLanguage(srcFile, collection, displayFiles) {
     fs.readdir(srcFile, (err, files) => {
-        console.log("Files: " + files.join(", "));
+        if (displayFiles) console.log("Files: " + files.join(", "));
         countLinesFiles(srcFile, files, collection);
-      })
+    })
 }
 
 countFileByLanguage(srcFilejs, totalCountsJs);
 countFileByLanguage(srcFileJava, totalCountsJava);
 countFileByLanguage(srcFileScala, totalCountsScala);
+countFileByLanguage(srcFileDotnet, totalCountsDotnet);
+countFileByLanguage(srcFileWords, totalCountsWords);
 
 function writeCount(fileName, content) {
-    fs.writeFile(fileName, content, function(err) {
+    fs.writeFile(fileName, content, function (err) {
+        if (err) return console.log(error);
+    });
+}
+
+function writeCountAll(fileName, content) {
+    fs.writeFile(fileName, content, function (err) {
         if (err) return console.log(error);
     });
 }
@@ -47,21 +60,85 @@ function sumFunc(x, y) {
     return x + y;
 }
 
-setTimeout(function() {
+setTimeout(function () {
+
+    var result = [];
+    let displayOrderd = true;
+
     reducedJs = totalCountsJs.reduce(sumFunc, 0);
     writeCount('jsCount.txt', reducedJs);
-    console.log("Javascript is: " + reducedJs)
-    
-    
+    result.push({
+        lan: "JS   ",
+        lines: reducedJs
+    });
+
+
+
     reducedJava = totalCountsJava.reduce(sumFunc, 0);
     writeCount('javaCount.txt', reducedJava);
-    console.log("Java is: " + reducedJava)
-    
+
+    result.push({
+        lan: "Java ",
+        lines: reducedJava
+    });
+
     reducedScala = totalCountsScala.reduce(sumFunc, 0);
     writeCount('scalaCount.txt', reducedScala);
-    console.log("Scala is: " + reducedScala)
+    result.push({
+        lan: "Scala",
+        lines: reducedScala
+    });
 
-    console.log("total is: "  + (reducedJs + reducedJava + reducedScala));
+    reducedDotnet = totalCountsDotnet.reduce(sumFunc, 0);
+    writeCount('dotnetCount.txt', reducedDotnet);
+    result.push({
+        lan: "CS   ",
+        lines: reducedDotnet
+    });
+
+    reducedWords = totalCountsWords.reduce(sumFunc, 0);
+    writeCount('wordsCount.txt', reducedWords);
+    result.push({
+        lan: "WD   ",
+        lines: reducedWords
+    });
+
+
+    if (displayOrderd) {
+        result.sort((a, b) => a.lines > b.lines);
+    }
+
+
+    let run = `
+# run it
+node ./count.js
+    `;
+
+    let activity = `
+# Activities
+* Write source code related to any topic
+* Comment the understanding part of it
+    `;
+
+    let purposes = `
+# Purposes
+* Reach to 100 lines daily
+* 2000 lines of source code monthly
+* 20 commits more
+`;
+
+    let enfore = `# Enforce
+* Algorithms
+* Code writing velocity
+`;
+
+    let countInfo = "# All count" + result.reduce((x, y) => x + "\n- " + y.lan + ":\t" + y.lines, "");
+    
+    result.forEach(x => console.log("*    " + x.lan + ":\t" + x.lines));
+    console.log("*    TOTAL:\t" + (reducedJs + reducedJava + reducedScala + reducedDotnet + reducedWords));
+    countInfo += "\n- TOTAL:\t" + (reducedJs + reducedJava + reducedScala + reducedDotnet + reducedWords);
+    countInfo += "\n" + run +"\n" + enfore +  activity +  purposes;
+    
+    writeCount('Readme.md', countInfo);
 
 }, 5000);
-
